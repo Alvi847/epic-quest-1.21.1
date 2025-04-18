ec7893808b15dda19d3589a55f2aba6a32d6bf2b
Alvi847 <alcepeda@ucm.es>
2025-04-18 23:55:01 +0200

    Initalisation completed

---
+++ src/main/java/net/alvi/epicquest/EpicQuestClient.java
@@ -0,0 +1,10 @@
+package net.alvi.epicquest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EpicQuestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/alvi/epicquest/EpicQuestDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alvi.epicquest;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EpicQuestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
